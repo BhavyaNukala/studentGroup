29a6f10a3df5d8c0a75429f6e9493c839cff0649
BhavyaNukala <31818558+BhavyaNukala@users.noreply.github.com>
2017-09-11 13:55:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+	StudentGroup a=new StudentGroup(3);
+	a.setStudents(23,"Bhavya",2);
+	StudentGroup b=new StudentGroup(2);
+	b.getStudents("Bhavya");
+	}
+}
